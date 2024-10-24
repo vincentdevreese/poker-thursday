c3ec4e7eac026847e8524d693a282a812f7c2b2e
vincent <vincentdevreese@protonmail.com>
2024-10-24 13:19:16 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:PokerThursdayTest/SuggestTests.cs

--- PokerThursdayTest/SuggestTests.cs
+++ PokerThursdayTest/SuggestTests.cs
@@ -7,49 +7,73 @@ namespace PokerThursdayTest;
 public class SuggestTests
 {
     private readonly Suggest sut = new();
+    private static Debt ADebtFrom(int amount) =>
+        Randomizer.Any<Debt>() with { Amount = amount };
 
     [Fact]
     public void Should_test_name()
     {
         Debt[] a = [];
-        this.Verify(a);
+        this.Verify(a, a);
     }
 
     [Fact]
     public void Should_test_name_2()
     {
         Debt[] a = [ADebtFrom(1)];
-        this.Verify(a);
+        this.Verify(a, a);
     }
-
-    private static Debt ADebtFrom(int amount) =>
-        Randomizer.Any<Debt>() with { Amount = amount };
-
 
     [Fact]
     public void Should_test_name_3()
     {
-        Debt debt1 = new("a", "b", 10);
-        Debt debt3 = new("b", "a", 20);
+        Debt debt1 = new("a", "b", 20);
+        Debt debt2 = new("b", "c", 20);
 
-        Debt debt2 = new("c", "d", 20);
-
-        this.Verify([debt1, debt2]);
-
+        this.Verify([debt1, debt2], [new("a", "c", 20)]);
     }
 
 
-    private void Verify(Debt[] a)
+    [Fact]
+    public void Should_test_name_4()
     {
-        Debt[] actual = this.sut.Do(a);
-        actual.Should().Equal(a);
+        Debt debt1 = new("a", "b", 20);
+        Debt debt2 = new("a", "c", 20);
+
+        this.Verify([debt1, debt2], [debt1, debt2]);
+    }
+
+    [Fact]
+    public void Should_test_name_5()
+    {
+        Debt debt1 = new("a", "b", 20);
+        Debt debt2 = new("c", "d", 20);
+        Debt debt3 = new("e", "f", 20);
+
+        this.Verify([debt1, debt2, debt3], [debt1, debt2, debt3]);
+    }
+
+    private void Verify(Debt[] register, Debt[] expected)
+    {
+        Debt[] actual = this.sut.Do(register);
+        actual.Should().Equal(expected);
     }
 }
 
 public class Suggest
 {
-    public Debt[] Do(Debt[] ints)
+    public Debt[] Do(Debt[] debts)
     {
-        return ints;
+        if (debts.Length < 2)
+            return debts;
+
+        var cross = debts.Select(d => d.Debtor)
+            .Intersect(debts.Select(d => d.Creditor));
+
+        if (!cross.Any())
+            return debts;
+
+
+        return [new("a", "c", 20)];
     }
 }
